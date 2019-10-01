948330fb38ce4a36be0e78d683f1d2b951d195fb
Shofiullah Babor <babu_12f@yahoo.com>
2019-10-01 22:03:03 +0600

    class --10 complete

---
+++ first.php
@@ -0,0 +1,50 @@
+<?php
+
+    // echo "hello php";
+
+    // print("hello php");
+
+    // Variable
+    // $x = 3;
+    // $y = 2;
+
+    // $sum = $x + $y;
+    // $sub = $x - $y;
+    // $mul = $x * $y;
+    // $div = $x / $y;
+    // $rem = $x % $y;
+
+    // echo "The sum is = " . $sum . "<br>";
+    // echo "The subtraction is = " . $sub . "<br>";
+    // echo $mul . "<br>";
+    // echo $div . "<br>";
+    // echo $rem . "<br>";
+
+    // $r = "Rayhan hossain";
+    // $s = "Samrat";
+
+    // echo $r ."-". $s . "<br>";
+
+    // // (x + y)^2 = x^2 + 2xy + y^2
+
+    // $h = ($x*$x) + 2 * $x * $y + ($y * $y);
+
+    // $a = 3;
+
+    // $a /= 10; // $a = $a - 10
+
+    // $a = 2;
+
+    // $a++;  // $a =  $a + 1
+
+    $a = 3;
+
+    $a--; //$a = $a - 1;
+    --$a;
+
+    ++$a;
+
+    echo $a;
+
+
+?>
---
+++ third.php
@@ -0,0 +1,100 @@
+<?php
+    // $number = 25;
+    // $number = $number / 10;
+    // $number = (int) $number;
+
+    // switch($number)
+    // {
+    //     case 10:
+    //     case 9:
+    //     case 8:
+    //         echo "A+";
+    //         break;
+    //     case 7:
+    //         echo "A";
+    //         break;
+    //     case 6:
+    //         echo "A-";
+    //         break;
+    //     case 5:
+    //         echo "B";
+    //         break;
+    //     case 4:
+    //         echo "C";
+    //         break;
+    //     case 3:
+    //         echo "D";
+    //         break;
+    //     case 2:
+    //     case 1:
+    //         echo "F";
+    //         break;
+    // }
+
+    // Array :> collection of variable
+    // $employees = [1,2,3,4,5,6,7,8,9];
+
+    // $employees = [];
+    // $employees[0] = 13;
+    // $employees[1] = 24;
+    // $employees[2] = 50;
+
+    // echo $employees[2] . "<br>";
+
+    // // $people = [];
+    // // $people['name'] = "mahi";
+    // $people = ['name'=> 'mahi', 'age' => 24];
+
+    // echo $people['name'] . "<br>";
+    // echo $people['age'];
+
+    
+    // $matrix = [
+    //     [1, 2, 3],
+    //     [4, 5, 6],
+    //     [7, 8, 9]
+    // ];
+
+    // echo $matrix[2][1];
+
+    // $person_1 = ['name' => 'adnan', 'age' => 10];
+    // $person_2 = ['name' => 'santo', 'age' => 15];
+    // $person_3 = ['name' => 'hasan', 'age' => 30];
+
+    // $people = [];
+
+    // $people[0] = $person_1;
+    // $people[1] = $person_2;
+    // $people[2] = $person_3;
+
+    // echo $people[1]['name'];
+
+    //========================================= Loop ======================
+    $name = "hasan";
+
+    echo $name . "<br>";
+
+    // for loop
+    // for( $n = 2;  $n <= 20 ; $n = $n + 2)
+    // {
+    //     echo $n . "<br>";
+    // }
+
+    // while loop
+
+    $n = 1;
+    while( $n <= 20 )
+    {
+        echo $n . "<br>";
+
+        $n = $n *2;
+    }
+
+    // next 
+
+
+
+
+
+
+?>
